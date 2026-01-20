fbd79486e11d4226c5da92f359ea670811588346
Michal Lysakowski <michaslysakowski@gmail.com>
2026-01-20 11:20:44 +0100

    Solution fix list comprehension

--- app/main.py
+++ app/main.py
@@ -8,14 +8,15 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for person in people:
-        person_to_add = Person(person["name"], person["age"])
-        if person.get("wife"):
-            person_to_add.wife = person["wife"]
-        elif person.get("husband"):
-            person_to_add.husband = person["husband"]
-        person_list.append(person_to_add)
+    person_list = [
+        Person(person["name"], person["age"])
+        for person in people
+    ]
+    for i, person_data in enumerate(people):
+        if person_data.get("wife"):
+            person_list[i].wife = person_data["wife"]
+        elif person_data.get("husband"):
+            person_list[i].husband = person_data["husband"]
 
     for person in person_list:
         if hasattr(person, "wife"):
